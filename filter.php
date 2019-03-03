f5f3456b97da2c9815070c0490648b6e97a89fc1
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-04 10:06:23 +1100

    FILTER and SANITIZE

---
+++ filter.php
@@ -0,0 +1,60 @@
+<?php
+
+   //Check for posted data
+  /* if(filter_has_var(INPUT_POST,'data')){
+      echo 'Data Found';
+   } else {
+      echo 'No Data';
+   } */
+   /*
+   //Check for Email Validation
+   if(filter_has_var(INPUT_POST,'data')){
+     $email = $_POST['data'];
+
+     //Remove illegal chars
+     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+     echo $email. '<br>';
+
+     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo 'Email is valid';
+    } else{
+        echo 'Email is NOT valid';
+    }
+   } 
+
+   // FILTER
+   FILTER_VALIDATE_BOOLEAN
+   FILTER_VALIDATE_EMAIL
+   FILTER_VALIDATE_FLOAT
+   FILTER_VALIDATE_INT
+   FILTER_VALIDATE_IP
+   FILTER_VLAIDATE_REGEXP
+   FILTER_VALIDATE_URL
+
+   // SANITIZE
+   FILTER_SANITIZE_EMAIL
+   FILTER_SANITIZE_ENCODED
+   FILTER_SANITIZE_NUMBER_FLOAT
+   FILTER_SANITIZE_NUMBER_INT
+   FILTER_SANITIZE_SPECIAL_CHARS
+   FILTER_SANITIZE_STRING
+   FILTER_SANITIZE_URL
+   
+
+   $var = 'sgajtu';
+
+   if(filter_var($var, FILTER_VALIDATE_INT)){
+       echo $var. 'is a number';
+   } else {
+       echo $var. 'is NOT a number';
+   }
+   */
+
+   $var = '33dlsfkjdlfjlsdjk4l343ldsjfldj';
+   var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+?>
+
+<form method= "post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="data">
+    <button type="submit"> Submit </button>
+</form>
