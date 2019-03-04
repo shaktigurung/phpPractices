fec631bc6f0fd379f671c792da094400f91edddd
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-04 16:18:17 +1100

    Filter and Validations

--- filter.php
+++ filter.php
@@ -48,13 +48,51 @@
    } else {
        echo $var. 'is NOT a number';
    }
-   */
-
+   
    $var = '33dlsfkjdlfjlsdjk4l343ldsjfldj';
-   var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+   $value = '<script>alert(helo) </script>';
+   //echo $value;
+   //var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+   echo filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
+
+   
+  $filters = array(
+    "data" => FILTER_VALIDATE_EMAIL,
+    "data2" => array(
+        "filter" => FILTER_VALIDATE_INT,
+        "options"=> array(
+            "min_range"=> 1,
+            "max_range"=> 100
+        )
+    )
+  );
+  print_r(filter_input_array(INPUT_POST,$filters));
+ */
+  $arr = array(
+      "name"=> "john doe",
+      "age"=> "125",
+      "email"=> "johndoe@test.com"
+  );
+
+  $filters = array(
+      "name" => array(
+          "filter" => FILTER_CALLBACK,
+          "options" => "ucwords"
+      ),
+      "age" => array(
+          "filter"=> FILTER_VALIDATE_INT,
+          "options"=> array(
+              "min_range"=> 1,
+              "max_range"=> 120
+          )
+          ),
+          "email" => FILTER_VALIDATE_EMAIL
+  );
+  print_r(filter_var_array($arr, $filters));
 ?>
 
 <form method= "post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <input type="text" name="data">
+    <input type="text" name="data2">
     <button type="submit"> Submit </button>
 </form>
